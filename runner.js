4f2ab46c8c7dae58f89633725974eae72bf9617a
Stephen Bradshaw <stephenbradshaw@Stephens-MacBook-Pro.local>
2013-06-18 22:55:21 +0100

    Add tests

--- runner.js
+++ runner.js
@@ -1,44 +1,65 @@
-var tests = [], scheduled, failed;
+var bind = require('bind-this');
 
-function runLoop (i, promise) {
-	i = i || 0;
-	if (promise) {
-		promise.then(function (fn) {
-			var test = tests[i - 1];
-			run(test.name, fn);
-			runner(i);
-		});
-	} else {
-		var test = tests[i];
-		if (!test) process.exit(failed ? 1 : 0);
-		promise = run(test.name, test.fn);
-		runner(++i, promise);
-	}
+function Runner (name, fn) {
+	this.tests = [];
+	this.scheduled = false;
+	this.failed = false;
 }
 
-function run (name, fn) {
+Runner.prototype.register = function (name, fn) {
+	this.tests.push({
+		name: name,
+		fn: fn
+	});
+};
+
+Runner.prototype.getTests = function () {
+	return this.tests;
+};
+
+Runner.prototype.runLoop = function (i, promise) {
+	i = i || 0;
+
+	if (promise) {
+		promise.then(bind(this, function (fn) {
+			var test = this.tests[i - 1];
+			this.runTest(test.name, fn);
+			this.runLoop(i);
+		}));
+	} else {
+		var test = this.tests[i];
+
+		if (!test) {
+			this.end();
+		}
+
+		promise = this.runTest(test.name, test.fn);
+		this.runLoop(++i, promise);
+	}
+};
+
+Runner.prototype.runTest = function (name, fn) {
 	var promise;
 
 	try {
 		promise = fn();
 	} catch (e) {
-		var err = failed = e;
+		var err = this.failed = e;
 	}
 
-	if (promise && !err) return promise;
+	if (promise && !err) {
+		return promise;
+	}
 
 	console.log((err ? '\x1B[31m' + '✖ Failed: ' : '\x1B[32m' + '✓ Passed: ') + '\x1B[37m' + name);
-	if (err) console.log('\n\u0009' + err + '\n');
-}
 
-module.exports = function (name, fn) {
-	tests.push({
-		name: name,
-		fn: fn
-	});
-
-	if (!scheduled) {
-		scheduled = true;
-		process.nextTick(runLoop);
+	if (err) {
+		console.log('\n\u0009' + err + '\n');
 	}
 };
+
+Runner.prototype.end = function () {
+	process.exit(this.failed ? 1 : 0);
+};
+
+module.exports = Runner;
--- test.js
+++ test.js
@@ -1,51 +1,60 @@
-var test = require('./runner');
+var test = require('./main');
 var assert = require('assert');
+var Runner = require('./Runner');
+
+test('can register tests', function () {
+	var runner = new Runner();
+
+	assert.equal(0, runner.getTests().length);
+	runner.register("test", function () {});
+	assert.equal(1, runner.getTests().length);
+});
 
 test('can pass tests', function () {
-	assert(true);
+	
 });
 
-test('can fail tests', function () {
-	assert(false);
-});
+// test('can fail tests', function () {
+// 	assert(false);
+// });
 
-test('blocks run loop when returning promises', function () {
-	var promise = {
-		then: function (fn) {
-			// Fake resolve and fail test
-			process.nextTick(function () {
-				fn(function () {
-					assert(true);
-				});
-			});
-		}
-	};
+// test('blocks run loop when returning promises', function () {
+// 	var promise = {
+// 		then: function (fn) {
+// 			// Fake resolve and fail test
+// 			process.nextTick(function () {
+// 				fn(function () {
+// 					assert(true);
+// 				});
+// 			});
+// 		}
+// 	};
 
-	return promise;
-});
+// 	return promise;
+// });
 
-test('can pass test with promise API', function () {
-	var promise = {
-		then: function (fn) {
-			// Fake resolve and pass test
-			fn(function () {
-				assert(true);
-			});
-		}
-	};
+// test('can pass test with promise API', function () {
+// 	var promise = {
+// 		then: function (fn) {
+// 			// Fake resolve and pass test
+// 			fn(function () {
+// 				assert(true);
+// 			});
+// 		}
+// 	};
 
-	return promise;
-});
+// 	return promise;
+// });
 
-test('can fail test with promise API', function () {
-	var promise = {
-		then: function (fn) {
-			// Fake resolve and fail test
-			fn(function () {
-				assert(false);
-			});
-		}
-	};
+// test('can fail test with promise API', function () {
+// 	var promise = {
+// 		then: function (fn) {
+// 			// Fake resolve and fail test
+// 			fn(function () {
+// 				assert(false);
+// 			});
+// 		}
+// 	};
 
-	return promise;
-});
+// 	return promise;
+// });
